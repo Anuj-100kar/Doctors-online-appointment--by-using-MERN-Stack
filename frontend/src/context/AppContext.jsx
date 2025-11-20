335b005487dad937377eb3d5180d814e232fcec0
Anuj-100kar <shambharkaranuj@gmail.com>
2025-11-20 18:57:04 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext=createContext();
 const AppContextProvider=(props)=>{
 
     const currencySymbol='$';
-    const backendUrl=import.meta.env.VITE_BACKEND_URL|| "http://localhost:4000"
+    const backendUrl=import.meta.env.VITE_BACKEND_URL|| "https://doctors-appointment-backend-wu75.onrender.com"
     const [doctors,setDoctors]=useState([])
     const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
     const [userData,setUserData]=useState(false)
